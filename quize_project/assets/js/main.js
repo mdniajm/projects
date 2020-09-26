f6b9027d013b7aeb973d5d60dfffd8f008269e6a
mdniajm <mdniajm@gmail.com>
2020-09-26 12:54:29 +0600

    add

---
+++ quize_project/assets/js/main.js
@@ -0,0 +1,252 @@
+$(document).ready(function() {
+    // grab the initial top offset of the navigation 
+       var stickyNavTop = $('nav').offset().top;
+       
+       // our function that decides weather the navigation bar should have "fixed" css position or not.
+       var stickyNav = function(){
+        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+             
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scrollTop > stickyNavTop) { 
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky'); 
+        }
+    };
+
+    stickyNav();
+    // and run it again every time you scroll
+    $(window).scroll(function() {
+        stickyNav();
+    });
+    $(document).ready(function(){
+
+ 
+   $(".drop-btn").click(function(){
+    $(".drop-down").toggleClass("drop");
+  });
+
+    window.onclick = function(event) {
+  if (!event.target.matches('.drop-btn')) {
+    var dropdowns = document.getElementsByClassName("drop-down");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('drop')) {
+        openDropdown.classList.remove('drop');
+      }
+    }
+  }
+};   
+});
+});
+
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".bgt");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll > 0) {
+            header.removeClass('bgt').addClass("bgd");
+        } else {
+            header.removeClass("bgd").addClass('bgt');
+        }
+    });
+});
+
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".none");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 600) {
+            header.removeClass('none').addClass("go-top");
+        } else {
+            header.removeClass("go-top").addClass('none');
+        }
+    });
+});
+
+
+
+     
+
+jQuery(document).ready(function( $ ) {
+        $('.counter').counterUp({
+            delay: 20,
+            time: 2000
+        });
+         new WOW().init();
+
+$(document).ready(function(){
+
+ 
+   $(".side-btn").click(function(){
+    $(".ul-down").toggleClass("ul-drop");
+  });
+
+    window.onclick = function(event) {
+  if (!event.target.matches('.side-btn')) {
+    var dropdowns = document.getElementsByClassName("ul-down");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('ul-drop')) {
+        openDropdown.classList.remove('ul-drop');
+      }
+    }
+  }
+};   
+});
+
+});
+
+
+// google chart
+$(document).ready(function() {
+   google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
+  });
+$(document).ready(function() {
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+
+  });
+$(document).ready(function() {
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Guardians of the Galaxy');
+      data.addColumn('number', 'The Avengers');
+      data.addColumn('number', 'Transformers: Age of Extinction');
+
+      data.addRows([
+        [1,  37.8, 80.8, 41.8],
+        [2,  30.9, 69.5, 32.4],
+        [3,  25.4,   57, 25.7],
+        [4,  11.7, 18.8, 10.5],
+        [5,  11.9, 17.6, 10.4],
+        [6,   8.8, 13.6,  7.7],
+        [7,   7.6, 12.3,  9.6],
+        [8,  12.3, 29.2, 10.6],
+        [9,  16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11,  5.3,  7.9,  4.7],
+        [12,  6.6,  8.4,  5.2],
+        [13,  4.8,  6.3,  3.6],
+        [14,  4.2,  6.2,  3.4]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Box Office Earnings in First Two Weeks of Opening',
+          subtitle: 'in millions of dollars (USD)'
+        },
+        width: 900,
+        height: 500,
+        axes: {
+          x: {
+            0: {side: 'top'}
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    };
+  });
+$(document).ready(function() {
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Guardians of the Galaxy');
+      data.addColumn('number', 'The Avengers');
+      data.addColumn('number', 'Transformers: Age of Extinction');
+
+      data.addRows([
+        [1,  37.8, 80.8, 41.8],
+        [2,  30.9, 69.5, 32.4],
+        [3,  25.4,   57, 25.7],
+        [4,  11.7, 18.8, 10.5],
+        [5,  11.9, 17.6, 10.4],
+        [6,   8.8, 13.6,  7.7],
+        [7,   7.6, 12.3,  9.6],
+        [8,  12.3, 29.2, 10.6],
+        [9,  16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11,  5.3,  7.9,  4.7],
+        [12,  6.6,  8.4,  5.2],
+        [13,  4.8,  6.3,  3.6],
+        [14,  4.2,  6.2,  3.4]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Box Office Earnings in First Two Weeks of Opening',
+          subtitle: 'in millions of dollars (USD)'
+        },
+        width: 900,
+        height: 500,
+        axes: {
+          x: {
+            0: {side: 'top'}
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x2'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    };
+  });
